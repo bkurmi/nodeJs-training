8d7b9c8d5d04d082c40eba567a8675054fbed4c5
BhaweshKurmi <bhawesh.kurmi@royalcyber.com>
2024-06-24 09:19:56 +0530

    Mongoose integration

--- TEMPLATE-NODEJS-PROJECT/controllers/product-controller.js
+++ TEMPLATE-NODEJS-PROJECT/controllers/product-controller.js
@@ -6,52 +6,56 @@ exports.saveProduct = (req, res, next) => {
   const productName = req.body.productName;
   const productDescription = req.body.productDescription;
   const product = new Product({
-    productPrice : productPrice,
-    productName : productName,
-    productDescription: productDescription
+    productPrice: productPrice,
+    productName: productName,
+    productDescription: productDescription,
   });
 
-  product.save()
-  .then(product => {
-    console.log(`Product saved successfully :: ${product.productName}`)
-  }).catch(err => {
-    console.log(err)
-  })
+  product
+    .save()
+    .then((product) => {
+      console.log(`Product saved successfully :: ${product.productName}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getProductById = (req, res, next) => {
   const productId = req.params.productId;
   Product.findById(productId)
-  .then(product=> {
-    console.log(`product found : ${product}`)
-  }).catch(err => {
-    console.log(err)
-  })
-}
+    .then((product) => {
+      console.log(`product found : ${product}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 exports.deleteProductById = (req, res, next) => {
   const productId = req.params.productId;
   Product.findOneAndDelete(productId)
-  .then(product=> {
-    console.log(`product Deleted Successfully`)
-  }).catch(err => {
-    console.log(err)
-  })
-}
+    .then((product) => {
+      console.log(`product Deleted Successfully`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 exports.updateProductById = (req, res, next) => {
   const productId = req.params.productId;
   Product.findById(productId)
-  .then(product => {
-    product.productName = req.body.productName;
-    product.productDescription = req.body.productDescription;
-    product.productPrice = req.body.productPrice;
-    console.log("updatedProduct : ", product);
-    return product.save()
-  })
-  .then (product => {
-    console.log("Updated Successfully")
-  }).catch(error => {
-    console.log("UPDATE FAILED", error);
-  });
-}
+    .then((product) => {
+      product.productName = req.body.productName;
+      product.productDescription = req.body.productDescription;
+      product.productPrice = req.body.productPrice;
+      return product.save();
+    })
+    .then((product) => {
+      console.log("Updated Successfully", product);
+    })
+    .catch((error) => {
+      console.log("UPDATE FAILED", error);
+    });
+};
